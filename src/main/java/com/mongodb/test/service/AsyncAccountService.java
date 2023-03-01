cb799a361cc75b0307390021c80ce716506182f1
jiachunyuan <chunyuan.jia@mongodb.com>
2023-03-01 20:28:11 +0800

    delete useless code

--- src/main/java/com/mongodb/test/service/AsyncAccountService.java
+++ src/main/java/com/mongodb/test/service/AsyncAccountService.java
@@ -79,7 +79,6 @@ public class AsyncAccountService {
                     }else {
                         transfer(clientSession, transfer, hasError, shard);
                     }
-                    transfer(clientSession, transfer, hasError, shard);
                     return null;
                 }
             };
